d21eafdb167ea9dd917d3fb6ef476cddfcfcdc66
Thawan trs3 <163412559+thawan-xyz@users.noreply.github.com>
2024-11-20 11:10:58 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define array vector
+#define read cin
+#define str string
+#define write cout
+#define endl '\n'
+using namespace std;
+
+/* # @author: Thawan Silva, 2024-08-14
+ * # @problem: [14] - Name
+ * # @url: https://github.com/thawan-xyz */
+
+int main() {
+    int t; read >> t;
+    while (t--) {
+        int n; read >> n;
+        array<int> a(n);
+        int s = 0;
+        for (int &x : a) {
+            read >> x;
+            s ^= x;
+        }
+        int m = INT_MAX;
+        for (int x : a) {
+            m = min(m, x ^ s);
+        }
+        write << m << endl;
+    }
+    return 0;
+}
